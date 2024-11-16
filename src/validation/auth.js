071cce839dc013ce60057e4662530100e9abe5a5
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-11-16 18:55:34 +0200

    Update validate

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,19 +1,21 @@
 import Joi from 'joi';
+import { themeType } from '../constants/themeType.js';
 
 export const registerUsersSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  name: Joi.string().min(2).max(32).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(8).max(64).required(),
 });
 
 export const loginUsersSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(8).max(64).required(),
 });
 
 export const updateUserSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
+  name: Joi.string().min(2).max(32),
   email: Joi.string().email(),
-  password: Joi.string(),
+  password: Joi.string().min(8).max(64),
   avatar: Joi.string(),
+  theme: Joi.string().valid(...themeType),
 });

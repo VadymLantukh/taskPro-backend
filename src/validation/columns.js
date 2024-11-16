3edc16ddda529f6b533e0eb96bda0d1109252e88
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-11-16 16:39:47 +0200

    validation fix

--- src/validation/columns.js
+++ src/validation/columns.js
@@ -1,7 +1,7 @@
 // validation/columnValidation.js
 import Joi from 'joi';
 
-const createColumnSchema = Joi.object({
+export const createColumnSchema = Joi.object({
   userId: Joi.string().required().messages({
     'string.empty': 'User ID is required',
   }),
@@ -13,14 +13,9 @@ const createColumnSchema = Joi.object({
   }),
 });
 
-const updateColumnSchema = Joi.object({
+export const updateColumnSchema = Joi.object({
   title: Joi.string().optional().messages({
     'string.empty': 'Title is required',
   }),
   tasks: Joi.array().items(Joi.string()).optional(),
 });
-
-export default {
-  createColumnSchema,
-  updateColumnSchema,
-};

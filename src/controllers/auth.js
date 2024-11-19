9d00552a5e888fdebec3bee0d44603d191452b03
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-11-19 10:02:08 +0200

    remove data in register user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -24,12 +24,11 @@ const setupSession = (res, session) => {
 };
 
 export const registerUsersController = async (req, res) => {
-  const user = await registerUser(req.body);
+  await registerUser(req.body);
 
   res.status(201).json({
     status: 201,
     message: 'Successfuly registered a user!',
-    data: user,
   });
 };
 

2a5cc9ffdbd76ac6ae9403d7330ff7f86d91844e
Gleb Yasynskyi <91141358+GYaskey@users.noreply.github.com>
2024-11-20 21:14:14 +0200

    swagger upd (#61)
    * swagger upd
    
    * upd swagger.json

--- src/validation/columns.js
+++ src/validation/columns.js
@@ -4,13 +4,13 @@ export const createColumnSchema = Joi.object({
   boardId: Joi.string().required().messages({
     'string.empty': 'Board ID is required',
   }),
-  title: Joi.string().required().messages({
+  title: Joi.string().min(3).max(30).required().messages({
     'string.empty': 'Title is required',
   }),
 });
 
 export const updateColumnSchema = Joi.object({
-  title: Joi.string().optional().messages({
+  title: Joi.string().min(3).max(30).optional().messages({
     'string.empty': 'Title is required',
   }),
   tasks: Joi.array().items(Joi.string()).optional(),

a66844792e61130cdeb2b20a072f85242db372ee
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-11-26 13:11:52 +0200

    Change validation auth

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,15 +1,16 @@
 import Joi from 'joi';
 
 import { themeType } from '../constants/themeType.js';
+import { emailRegexp } from '../constants/emailRegexp.js';
 
 export const registerUsersSchema = Joi.object({
   name: Joi.string().min(2).max(32).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(8).max(64).required(),
 });
 
 export const loginUsersSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(8).max(64).required(),
 });
 
